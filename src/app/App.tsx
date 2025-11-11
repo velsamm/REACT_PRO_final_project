36a5c49db69bd7fb1515c39c1d2de6443fb7a5d1
maelstromm <email@email.cpm>
2025-11-11 13:26:22 +0300

    добавлены абсолютные испорты

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,10 @@ import './styles/styles.css';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../widgets/Header';
 import { Sort } from '../shared/ui/Sort';
-import { Footer } from '../widgets/Footer';
+// import { Footer } from '@widgets/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Footer } from '@widgets/Footer';
 
 export const App = () => {
 	return (
